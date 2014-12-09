a00da16d44288d71fed65bb66a3f288a79cf4afb
sooheon <tngjs0@gmail.com>
2014-12-09 11:41:18 +0900

    Getting started

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,14 +3,29 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (if (zero? (value-at board coord))
+    false
+    true))
 
 (defn row-values [board coord]
-  nil)
+  )
 
 (defn col-values [board coord]
   nil)
